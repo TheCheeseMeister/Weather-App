38d00807215c602d01cd26c2740a8653c007c5bd
TheCheeseMeister <ssomething511@gmail.com>
2024-08-04 16:32:37 -0400

    setup base stuff i gota go

---
+++ index.js
@@ -0,0 +1,17 @@
+// [location]/[date1]/[date2]?key=YOUR_API_KEY
+const baseLink = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+const apiKey = 'TGLBQYWMH8JGJB32WSP2U4T8Z';
+
+const text = document.getElementById('location');
+const btn = document.getElementById('submit');
+btn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    searchLocation(text.value);
+
+    text.value = '';
+});
+
+function searchLocation(location) {
+    console.log(location);
+}
